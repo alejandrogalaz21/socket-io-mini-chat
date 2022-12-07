de04f40e0ce0bd4a6c0f40dfceb7b324d3f2c818
alejandro galaz <alejandrogalaz21@gmail.com>
2022-12-07 16:11:48 -0600

    enviando y recibiendo mensajes

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,22 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', socket => {
   console.log('a user connected :', socket.id)
-  socket.emit('welcome-message', {
+  // emit welcome message
+  socket.emit('server-message', {
     message: `welcome user you id is : ${socket.id}`,
     date: new Date()
+  })
+
+  // listen message event
+  socket.on('client-message', data => {
+    console.log({ data })
+    const { message, date } = data
+    // emit messages to all clients
+    socket.emit('server-emit-messages', {
+      socketId: socket.id,
+      message,
+      date
+    })
   })
 })
 
