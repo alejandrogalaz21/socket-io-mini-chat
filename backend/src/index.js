4690515704e3ccdecb12885e6e27ebb510cf0b46
alejandro galaz <alejandrogalaz21@gmail.com>
2022-12-07 15:09:33 -0600

    render public dir

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import http from 'http'
 import express from 'express'
 import { Server } from 'socket.io'
+import path from 'path'
+
 // Express App
 const app = express()
 // Server
@@ -8,9 +10,7 @@ const server = http.createServer(app)
 // Socket.io Server
 const io = new Server(server)
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>')
-})
+app.use(express.static(path.join(__dirname, '../public')))
 
 io.on('connection', socket => {
   console.log('a user connected')
