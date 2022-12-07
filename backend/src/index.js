6e3d5933c1ea37f2371caf4843c66a1d715cb6cc
alejandro galaz <alejandrogalaz21@gmail.com>
2022-12-07 14:58:49 -0600

    initial config server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,21 @@
-const lits = [1, 2, 3, 4]
+import http from 'http'
+import express from 'express'
+import { Server } from 'socket.io'
+// Express App
+const app = express()
+// Server
+const server = http.createServer(app)
+// Socket.io Server
+const io = new Server(server)
 
-lits.forEach(element => {
-  console.log(element)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
+})
+
+server.listen(3000, () => {
+  console.log('listening on *:3000')
 })
