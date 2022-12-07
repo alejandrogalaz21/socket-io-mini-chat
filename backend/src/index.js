07669e7c9312ccf4ddf95b31dcf2fb2ca0cbd934
alejandro galaz <alejandrogalaz21@gmail.com>
2022-12-07 16:52:34 -0600

    finish repo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv'
 import Server from './models/server'
-
+// set global env variables
+dotenv.config()
+// instance of the server
 const server = new Server()
+// run the server
 server.execute()
